2fc0480ae6810590fe2e37fe12810bfd6ef4a988
pedro neto <pedro.neto72pn@gmail.com>
2021-05-14 13:09:32 -0300

    botao backToTop retirado

--- public/scripts.js
+++ public/scripts.js
@@ -26,29 +26,6 @@ const swiper = new Swiper('.swiper-container', {
 
 });
 
-mybutton = document.getElementById("bttbutton");
-
-window.onscroll = function() {
-  scrollFunction()
-};
-
-
-//Mostra o botão quando move 100px para baixo do topo
-function scrollFunction() {
-  if (document.body.scrollTop > 100 //para safari
-    || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-//Quando clica no botão volta para o topo
-function backToTop() {
-  document.body.scrollTop = 0; //para safari
-  document.documentElement.scrollTop = 0;
-}
-
 navbar = document.getElementById("desktop-navbar");
 window.onscroll = function() {
   navbarFunction()
